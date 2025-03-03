0fc828ef4721bff823129ef532166ae5db5f0674
Nine9844 <984478169@qq.com>
2025-03-03 13:07:51 +0800

    数组中最长连续序列

--- lk/hot100/twoSum.go
+++ lk/hot100/twoSum.go
@@ -1,19 +1,32 @@
 package hot100
 
 func TwoSum(nums []int, target int) []int {
-	l := len(nums)
-	var ans []int
-	for i := 0; i < l-1; i++ {
-		for j := i + 1; j < l; j++ { // 从 i+1 开始，并且检查到数组的末尾
-			if i == j {
-				continue
-			}
-			if nums[i]+nums[j] == target {
-				ans = append(ans, i)
-				ans = append(ans, j)
-				return ans // 找到结果后直接返回，提高效率
-			}
+	//官方中哈希表解法
+	hashMap := map[int]int{}
+	for i, x := range nums {
+		//使用差值来进行比较
+		if p, ok := hashMap[target-x]; ok {
+			return []int{p, i}
 		}
+		hashMap[x] = i
 	}
-	return ans // 如果没有找到结果，返回空数组
+	return nil
 }
+
+//// 个人解题思路：暴力解法
+//l := len(nums)
+//var ans []int
+//for i := 0; i < l-1; i++ {
+//	for j := i + 1; j < l; j++ { // 从 i+1 开始，并且检查到数组的末尾
+//		if i == j {
+//			continue
+//		}
+//		if nums[i]+nums[j] == target {
+//			ans = append(ans, i)
+//			ans = append(ans, j)
+//			return ans // 找到结果后直接返回，提高效率
+//		}
+//	}
+//}
+//return ans // 如果没有找到结果，返回空数组
+//}
--- lk/main.go
+++ lk/main.go
@@ -4,6 +4,13 @@ import "fmt"
 import "lk/hot100"
 
 func main() {
+
+	//最长连续序列
+	nums1 := []int{100, 4, 200, 1, 3, 2}
+	result1 := hot100.LongestConsecutive(nums1)
+	fmt.Println("long:", result1)
+
+	//两数之和等于目标数
 	nums := []int{2, 7, 11, 15}
 	target := 9
 	result := hot100.TwoSum(nums, target)
